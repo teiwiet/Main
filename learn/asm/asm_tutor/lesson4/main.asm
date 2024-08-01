84600b0a3db0fb23f33a4220f30b9b09f5e2ff23
teiwiet <omegalmao1.3@gmail.com>
2024-08-01 10:35:57 -0400

    update

---
+++ learn/asm/asm_tutor/lesson4/main.asm
@@ -0,0 +1,26 @@
+section .data 
+message db "Hello World!, my name is Viet",0xA,0
+
+section .text 
+global _start
+
+_start:
+	mov eax,message 
+	mov ebx,eax
+
+update:
+	cmp byte [eax],0
+	jz write_char 
+	inc eax 
+	jmp update
+
+write_char:
+	sub eax,ebx
+	mov edx,eax
+	mov eax,4
+	mov ebx,1
+	mov ecx,message
+	int 0x80
+	mov eax,1
+	mov ebx,0
+	int 0x80
