19e1dcefba21ab3230c14882c6ac4d1454f73301
teiwiet <omegalmao1.3@gmail.com>
2024-08-25 05:36:16 -0400

    update

--- learn/asm/asm_tutor/lesson5/functions.asm
+++ learn/asm/asm_tutor/lesson5/functions.asm
@@ -1,36 +1,41 @@
+; exit() function
+exit:
+	mov eax,1
+	mov ebx,0 
+	int 0x80 
+	ret 
+
+;strlen(char* msg) 
+strlen:
+	push ebx 
+	mov ebx,eax 
+next_char:
+	cmp byte[eax],0
+	jz finished
+	inc eax 
+	jmp next_char 
+finished:
+	sub eax,ebx 
+	pop ebx 
+	ret 
+
+;print(char* msg) 
 print:
 	push edx 
-	push ecx 
 	push ebx 
+	push ecx 
 	push eax 
-	call strlen 
-	
+	call strlen
+
 	mov edx,eax 
 	pop eax 
 	mov ecx,eax 
 	mov ebx,1 
 	mov eax,4 
-	int 0x80
+	int 0x80 
 
-	pop ebx 
 	pop ecx 
+	pop ebx 
 	pop edx 
 	ret
-; void calc str length 
-strlen:
-	push ebx 
-	mov ebx,eax 
-update_char:
-	cmp byte [eax],0
-	jz finish 
-	inc eax
-	jmp update_char
-finish:
-	sub eax,ebx 
-	pop ebx 
-	ret
-quit:
-	mov eax,1 
-	mov ebx,0 
-	int 0x80 
-	ret
+
--- learn/asm/asm_tutor/lesson5/main.asm
+++ learn/asm/asm_tutor/lesson5/main.asm
@@ -1,15 +1,15 @@
-%include "functions.asm" 
-section .data
-msg1 db "Hello, brave new world",0xA 
-msg2 db "This is how we recycle in nasm",0xA 
+%include 'functions.asm'
 
-section .text 
+section .data
+msg db "Hello World",0xa,0 
+msg1 db "Hello World no.2",0xa,0
+section .text
 global _start
 _start:
-	mov eax,msg1 
+	mov eax,msg 
+	call print
+	 
+	mov eax,msg1
 	call print
 
-	mov eax,msg2
-	call print 
-
-	call quit ; exit func
+	call exit
