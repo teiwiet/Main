783b7d033672d3a67439611568b8c5f182846afd
teiwiet <omegalmao1.3@gmail.com>
2024-12-23 17:24:10 +0000

    update

--- learn/Os3/boot.asm
+++ learn/Os3/boot.asm
@@ -1,11 +1,20 @@
 org 0x00 
 bits 16 
 section code 
-.main:
+.init:
 	mov eax,0xb800
 	mov es,eax 
+	xor eax,eax
+.clear:
+	mov byte[es:eax],0
+	inc eax
+	mov byte[es:eax],0x30
+	cmp eax,2*25*80
+	jl .clear
+.main:
 	mov byte [es:0x00],"H"
-	mov byte [es:0x01],0x10 
+	mov byte [es:0x01],0x30 
+	mov byte [es:0x02],0x30 
 
 jmp $
 times 510-($-$$) db 0 
