1fdfdcca6a35fd6cc18d70cd81a54125647eebb9
teiwiet <omegalmao1.3@gmail.com>
2025-01-17 19:46:41 +0000

    update

--- learn/Os3/boot.asm
+++ learn/Os3/boot.asm
@@ -1,22 +1,83 @@
-org 0x00 
-bits 16 
+[org 0x7c00]
+[bits 16]
 section code 
-.init:
+init:
 	mov eax,0xb800
 	mov es,eax 
 	xor eax,eax
-.clear:
+	xor ebx,ebx
+	xor ecx,ecx
+	xor dl,dl 
+clear:
 	mov byte[es:eax],0
 	inc eax
-	mov byte[es:eax],0x30
+	mov byte[es:eax],0xb0
+	inc eax
 	cmp eax,2*25*80
-	jl .clear
-.main:
-	mov byte [es:0x00],"H"
-	mov byte [es:0x01],0x30 
-	mov byte [es:0x02],0x30 
+	jl clear
+	push end
+	call print 
+end:
+	jmp $
+print:
+	mov eax,text 
+	mov dl,byte[eax+ebx]
+	cmp dl,0 
+	je end_print
+	mov byte[es:ecx],dl
+	inc ecx 
+	inc ecx
+	inc ebx
+	jmp print
+end_print:
+	ret
+switch:
+	cli 
+	lgdt[gdt_descriptor]
+	mov eax,cr0
+	or eax,0x1
+	mov cr0,eax 
+	jmp protected_start
+[bits 32]
+protected_start:
+	mov ax,data_seg
+	mov ds,ax
+	mov ss,ax
+	mov es,ax
+	mov fs,ax
+	mov gs,ax
 
-jmp $
+	mov ebp,0x90000
+	mov esp,ebp 
+	jmp $
+gdt_begin:
+gdt_null:
+	dd 0x00 
+	dd 0x00 
+gdt_code_segment:
+	dw 0xffff 
+	dw 0x0000
+	db 0x00 
+	db 10011010b
+	db 11001111b 
+	db 0x00 
+gdt_data_segment:
+	dw 0xffff 
+	dw 0x0000
+	db 0x00 
+	db 10010010b
+	db 11001111b 
+	db 0x00 
+gdt_end:
+gdt_descriptor:
+	dw gdt_end - gdt_begin -1 
+	dd gdt_begin
+
+code_seg equ gdt_begin - gdt_code_segment
+data_seg equ gdt_begin - gdt_data_segment
+
+text: db "Hello World",0
+text1: db "This is another text",0
 times 510-($-$$) db 0 
 db 0x55
 db 0xaa
