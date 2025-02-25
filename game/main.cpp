f7691ff22ca8517139ebd6d1d1ec1282a03f7452
teiwiet <teiwiet33@gmail.com>
2025-02-25 21:23:33 +0700

    update

---
+++ game/main.cpp
@@ -0,0 +1,9 @@
+#include<SDL2/SDL.h>
+#include<iostream>
+#include<SDL2/SDL_audio.h>
+using namespace std;
+
+int main(int argc,char* argv[]){
+
+	return 0;
+}
--- learn/stuff/main.c
+++
@@ -1,6 +0,0 @@
-#include<unistd.h>
-#include<string.h>
-int main(){
-	char* lmao = "Hello World";
-	write(1,lmao,strlen(lmao));
-}
