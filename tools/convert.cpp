716351e8dd95e331398333d05a5afaf766fed0c3
teiwiet <omegalmao1.3@gmail.com>
2025-03-13 06:50:58 +0000

    update

--- tools/convert.cpp
+++ tools/convert.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<string.h>
+#include<stdint.h>
 using namespace std;
-
+unsigned int a = 10;
+uint32_t a = 10;
 void printRed(char* s){
 	cout << "\033[31m" << s << "\033[0m";
 }
