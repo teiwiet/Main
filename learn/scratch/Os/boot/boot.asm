f2f37b00b178f15527d4e600b16d037d27c648f5
teiwiet <teiwiet33@gmail.com>
2025-05-18 13:54:05 +0700

    update

--- learn/scratch/Os/boot/boot.asm
+++ learn/scratch/Os/boot/boot.asm
@@ -1,46 +1,49 @@
+[bits 16] 
 [org 0x7c00] 
-[bits 16]
 switch:
-	mov ax,0x4f01 			; querring the VBE 
-	mov cx,0x117 			; Mode 
-	mov bx,0x08000			; Offset for the vbe infor structure
-	mov es,bx 
-	mov di,0x00 
+	mov ax,0x4f01 			
+	mov cx,0x117
+	push dx 
+	push dx
+	xor dx,dx 
+	mov dx,0x0800
+	mov es,dx 
+	pop dx
+	mov di,0x00
 	int 0x10
-
-	; Make switch to graphics mode 
+	; make the switch to graphics mode 
 	mov ax,0x4f02 
 	mov bx,0x117 
 	int 0x10
-
+	
 	xor ax,ax 
 	mov ds,ax 
 	mov es,ax 
 
+	mov bx,0x1000 			; location where the code load from hard disk
+	mov ah,0x02 
+	mov al,1				; number of sector to read from hard disk 
+	mov ch,0x00
+	mov dh,0x00
+	mov cl,0x02
+	int 0x13
 
-	mov ah,0x2 
-	mov al,1
-	mov ch,0x00 
-	mov cl,0x2 
-	mov bx,0x1000
-	int 0x13 
-
-	cli 
-	lgdt[gdt_descriptor]
-	mov eax,cr0
-	or eax,0x1 
-	mov cr0,eax 
+	cli  					; clear the the interrupt
+	lgdt[gdt_descriptor] 	; load the address to GDTR register 
+	mov eax,cr0 
+	or eax,1
+	mov cr0,eax				; make the switch
 	jmp code_seg:protected_start 
-[bits 32] 
+[bits 32]
 protected_start:
 	mov ax,data_seg 
 	mov ds,ax 
-	mov gs,ax 
-	mov fs,ax 
 	mov es,ax 
 	mov ss,ax 
+	mov fs,ax 
+	mov gs,ax 
 
-	mov ebp,0x90000 
+	mov ebp,0x90000 	; update the stack pointer
 	mov esp,ebp
 
 	call 0x1000
@@ -55,20 +58,21 @@ gdt_code_seg:
 	db 0x00 
 	db 10011010b
 	db 11001111b
-	db 0x00
+	db 0x00 
 gdt_data_seg:
 	dw 0xffff 
 	dw 0x00 
 	db 0x00 
 	db 10010010b
 	db 11001111b
-	db 0x00
+	db 0x00 
 gdt_end:
 gdt_descriptor:
-	dw gdt_end - gdt_begin - 1 
+	dw gdt_end - gdt_begin - 1
 	dd gdt_begin 
 code_seg equ gdt_code_seg - gdt_begin 
 data_seg equ gdt_data_seg - gdt_begin 
-
+text: db "hello world",0
 times 510-($-$$) db 0 
-dw 0xaa55 
+db 0x55 
+db 0xaa
