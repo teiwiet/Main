7945f3dbed7f101b3dcdfff83e9549dc2586fd89
teiwiet <teiwiet33@gmail.com>
2025-05-21 19:45:09 +0700

    update

--- stuffs/convert.cpp
+++ stuffs/convert.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
-#include<string.h>
 #include<stdint.h>
+#include<string.h>
 using namespace std;
-unsigned int a = 10;
-uint32_t a = 10;
-void printRed(char* s){
+void printRed(const string& s){
 	cout << "\033[31m" << s << "\033[0m";
 }
 int main(int argc,char** argv){
