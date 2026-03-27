bb38aebff23098ed6eb16183bb16a7e4940ee86f
teiwiet <teiwiet33@gmail.com>
2026-03-27 13:55:03 +0700

    update

--- stuffs/printColor.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-
-int main(){
-  cout << "\033[31mChữ này màu đỏ\033[0m" << endl;
-}
--- stuffs/test.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<fstream>
-using namespace std; 
-
-int main(){
-	int n;
-	cin >> n;
-	int res = 0;
-}
